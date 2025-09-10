51da6e79e50586631e2360ce5506761846872be6
Code_with_Manav <codewithmanav70@gmail.com>
2025-09-10 10:59:10 +0530

    upvote, downvote and filtering

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from sqlalchemy import select
+from sqlalchemy import Date, select
 from .database import engine, Base, get_session
 from .models import Confession
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
 
 app = FastAPI(title="T-Whispers API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
@@ -24,3 +34,34 @@ def add_confession(content: str, session: Session = Depends(get_session)):
     session.commit()
     session.refresh(new_confession)
     return new_confession
+
+@app.post("/confessions/{confession_id}/upvote")
+def upvote_confession(confession_id: int, session: Session = Depends(get_session)):
+    confession = session.get(Confession, confession_id)
+    if not confession:
+        raise HTTPException(status_code=404, detail="Confession not found")
+    confession.upvotes += 1
+    session.commit()
+    session.refresh(confession)
+    return {"id":confession.id, "upvotes": confession.upvotes, "downvotes": confession.downvotes}
+
+@app.post("/confessions/{confession_id}/downvote")
+def downvote_confession(confession_id: int, session: Session = Depends(get_session)):
+    confession = session.get(Confession, confession_id)
+    if not confession:
+        raise HTTPException(status_code=404, detail="Confession not found")
+    confession.downvotes += 1
+    session.commit()
+    session.refresh(confession)
+    return {"id":confession.id, "upvotes": confession.upvotes, "downvotes": confession.downvotes}
+
+@app.get("/confession/filter")
+def filter_confessions(date: str, session: Session = Depends(get_session)):
+    try:
+        target_date = datetime.strptime(date, "%Y-%m-%d").date()
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid date format. Use YYYY-MM-DD.")
+
+    result = session.query(Confession).filter(Confession.created_at.cast(Date) == target_date).order_by(Confession.created_at.desc()).all()
+
+    return result
